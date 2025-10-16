a418825294a63c54028891900f3fb26df5f1f60c
futshjhb025 <futshjhb025@student.wethinkcode.co.za>
2025-10-16 15:50:43 +0200

    first test

--- units.py
+++ units.py
@@ -1,8 +1,48 @@
-def convert(ratios, from_unit, to_unit, value):
+ratios = {
+    ("gleep", "glorp",): 3, # 3 gleeps = 1 glorp
+    ("shneep", "glorp"): 60, # 60 shneeps = 1 glorp
+}
+def convert(ratios, from_unit, to_unit, palue):
     if from_unit == to_unit:
-        return value
+        return palue
 
     if (from_unit, to_unit) in ratios:
-        return value / ratios[(from_unit, to_unit)]
+        return palue / ratios[(from_unit, to_unit)]
+    
+    for i, j in ratios.items():
+        if i[-2] == from_unit:
+            for p, l in ratios.items():
+                if p[-2] == to_unit:
+                    return int((l/j) * palue)
+                
+        #print(i[-1])
+                
+        if i[-1] == from_unit:
 
+            return int(j*palue)
+            #print("here",j)
+
+
+            # for p, l in ratios.items():
+            #     if p[-1] == to_unit:
+            #         return int(j * palue)
+                
+
+
+    # if (from_unit, to_unit) in ratios:
+    #     return palue / ratios[(to_unit, from_unit)]  
+       
+    # for t, s in ratios.items():
+    #     if t[0] == from_unit:
+    #         for b, q in ratios.items():
+    #            if b[0] == to_unit:
+    #                 return int(s* palue)
+    
+    
     return None
+
+print(convert(ratios, "glorp", "gleep", 2))
+
+
+
+
