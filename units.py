bf153972fc69529881a5f87baac3d5c456346bd4
futshjhb025 <futshjhb025@student.wethinkcode.co.za>
2025-10-20 15:44:05 +0200

    testing

--- units.py
+++ units.py
@@ -1,15 +1,11 @@
-ratiosw = {
-    ("gleep", "glorp",): 3, # 3 gleeps = 1 glorp
-    ("shneep", "glorp"): 60, # 60 shneeps = 1 glorp
+ratios = {
+    ("millimeter", "centimeter"): 10, # 10 millimeters = 1 centimeter
+    ("centimeter", "meter"): 100, # 100 centimeters = 1 meter
+    ("meter", "decameter"): 10, # 10 meters = 1 decameter
+    ("decameter", "hectometer"): 10, # 10 decameters = 1 hectometer
+    ("hectometer", "kilometer"): 10, # 10 hectometers = 1 kilometer
 }
 
-ratiosr = {
-            ("millimeter", "centimeter"): 10, # 10 millimeters = 1 centimeter
-            ("centimeter", "meter"): 100, # 100 centimeters = 1 meter
-            ("meter", "decameter"): 10, # 10 meters = 1 decameter
-            ("decameter", "hectometer"): 10, # 10 decameters = 1 hectometer
-            ("hectometer", "kilometer"): 10, # 10 hectometers = 1 kilometer
-        }
 def convert(ratios, from_unit, to_unit, palue):
     if from_unit == to_unit:
         return palue
@@ -19,47 +15,38 @@ def convert(ratios, from_unit, to_unit, palue):
     
     if (to_unit, from_unit) in ratios:
         return palue * ratios[(to_unit, from_unit)]
-    mult = 1
+    
+    
     for i, j in ratios.items():
-        print(i,j)
-        print(i[-2])
-        print(i[-1])
-
         if i[-2] == from_unit:
             for p, l in ratios.items():
-                if p[-1] == to_unit:
-                    return int((l/j) * palue) *mult
-                else:
-                    mult *= l
-
+                if p[-2] == to_unit:
+                   return int((l/j) * palue)
                 
-        #print(i[-1])
-                
-        if i[-1] == from_unit:
+# ratios = {
+#     ("millimeter", "centimeter"): 10, # 10 millimeters = 1 centimeter
+#     ("centimeter", "meter"): 100, # 100 centimeters = 1 meter
+#     ("meter", "decameter"): 10, # 10 meters = 1 decameter
+#     ("decameter", "hectometer"): 10, # 10 decameters = 1 hectometer
+#     ("hectometer", "kilometer"): 10, # 10 hectometers = 1 kilometer
+# }
 
-            return int(j*palue)
-            #print("here",j)
-
-
-            # for p, l in ratios.items():
-            #     if p[-1] == to_unit:
-            #         return int(j * palue)
-                
-
-
-    # if (from_unit, to_unit) in ratios:
-    #     return palue / ratios[(to_unit, from_unit)]  
-       
-    # for t, s in ratios.items():
-    #     if t[0] == from_unit:
-    #         for b, q in ratios.items():
-    #            if b[0] == to_unit:
-    #                 return int(s* palue)
-    
+    store_value = 1
+    target_unit = to_unit
+    for t, s in ratios.items():
+        if t[0] == target_unit:
+            store_value = store_value * s
+            target_unit = t[-1]
+        if target_unit == from_unit:
+            return palue * store_value
+        
     
     return None
 
-print(convert(ratios, "millimeter", "kilometer", 2))
+
+print(convert(ratios, "kilometer", "millimeter", 2))
+
+
 
 
 
